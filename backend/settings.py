934a0ed5b759671da5482726781e6d547952ed89
S-Ashique <aashiquee13@gmail.com>
2025-01-25 14:27:20 +0530

    auth

--- backend/settings.py
+++ backend/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'authentication',
 ]
 
 MIDDLEWARE = [
@@ -135,6 +137,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+AUTH_USER_MODEL = 'authentication.User'
 
 # Email
 EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND')
